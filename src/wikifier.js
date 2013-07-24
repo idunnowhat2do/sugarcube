0e29ed3955109c8efae927d23c279c59ab0eadb1
Thomas Edwards <tmedwards@motoslave.net>
2013-07-23 23:39:43 -0500

    * Fixed an issue with the <String>.readMacroParams() method that   caused variable substitution to fail on object variables. * Commented out the HTML character reference formatter, as I'm   not sure that it's necessary.

--- src/wikifier.js
+++ src/wikifier.js
@@ -48,15 +48,31 @@ String.prototype.readMacroParams = function (replaceVars)
 				// Variable, so perform substitution
 				if (replaceVars && /\$/.test(n))
 				{
-					var   varRe    = new RegExp("\\$(\\w+)", "g")
+					var   varRe    = new RegExp("\\$(\\w+)((?:\\.\\w+|\\[[^\\]]+\\])*)", "g")
 						, varText  = n
 						, varMatch;
 					while ((varMatch = varRe.exec(varText)) !== null)
 					{
-						var varName = varMatch[1];
+						var   varText  = varMatch[0]
+							, varName  = varMatch[1];
 						if (typeof state.active.variables[varName] !== "undefined")
 						{
-							n = n.replace("$" + varName, state.active.variables[varName]);
+							if (varMatch[2])
+							{
+								try
+								{
+									var objPropVal = eval(varText.replace(/\$/g, "state.active.variables."));
+									n = n.replace(varText, objPropVal);
+								}
+								catch (e)
+								{
+									// noop
+								}
+							}
+							else
+							{
+								n = n.replace(varText, state.active.variables[varName]);
+							}
 						}
 					}
 				}
@@ -980,15 +996,15 @@ Wikifier.formatters =
 			w.nextMatch = this.lookaheadRegExp.lastIndex;
 		}
 	}
-},
-
-{
-	name: "htmlEntitiesEncoding",
-	match: "(?:(?:&#?[a-zA-Z0-9]{2,8};|.)(?:&#?(?:x0*(?:3[0-6][0-9a-fA-F]|1D[c-fC-F][0-9a-fA-F]|20[d-fD-F][0-9a-fA-F]|FE2[0-9a-fA-F])|0*(?:76[89]|7[7-9][0-9]|8[0-7][0-9]|761[6-9]|76[2-7][0-9]|84[0-3][0-9]|844[0-7]|6505[6-9]|6506[0-9]|6507[0-1]));)+|&#?[a-zA-Z0-9]{2,8};)",
-	handler: function(w)
-	{
-		insertElement(w.output, "span").innerHTML = w.matchText;
-	}
+//},
+//
+//{
+//	name: "htmlEntitiesEncoding",
+//	match: "(?:(?:&#?[a-zA-Z0-9]{2,8};|.)(?:&#?(?:x0*(?:3[0-6][0-9a-fA-F]|1D[c-fC-F][0-9a-fA-F]|20[d-fD-F][0-9a-fA-F]|FE2[0-9a-fA-F])|0*(?:76[89]|7[7-9][0-9]|8[0-7][0-9]|761[6-9]|76[2-7][0-9]|84[0-3][0-9]|844[0-7]|6505[6-9]|6506[0-9]|6507[0-1]));)+|&#?[a-zA-Z0-9]{2,8};)",
+//	handler: function(w)
+//	{
+//		insertElement(w.output, "span").innerHTML = w.matchText;
+//	}
 }
 
 ];	// End formatters
