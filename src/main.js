9bbda1effe5b2ace1d9069eb18947339b1e587f1
Thomas Edwards <tmedwards@motoslave.net>
2013-08-21 14:13:56 -0500

    RELEASE: BETA-1934

--- src/main.js
+++ src/main.js
@@ -27,7 +27,7 @@ function getRandomArbitrary(min, max)
 /***********************************************************************************************************************
 ** [Initialization]
 ***********************************************************************************************************************/
-var version = { title: "SugarCube", major: 1, minor: 0, revision: 0, date: new Date("August 18, 2013"), extensions: {} };
+var version = { title: "SugarCube", major: 1, minor: 0, revision: 0, date: new Date("August 21, 2013"), extensions: {} };
 
 var modes =		// SugarCube History class modes
 {
