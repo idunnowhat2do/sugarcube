cb70946ea4a49e43df48b839b75017919ad9574f
Thomas Edwards <tmedwards@motoslave.net>
2014-05-01 16:53:41 -0500

    Updated the python header support file to return an "unsupported" message within the StorySettings UI and to apply coloring to the titles of both widget tagged passages and the SugarCube-specific special passages.

--- src/sugarcube.py
+++ src/sugarcube.py
@@ -7,7 +7,7 @@ class Header (header.Header):
 		return OrderedDict()
 
 	def storySettings(self):
-		return []
+		return "SugarCube does not support the StorySettings special passage.\n\nInstead, you should use SugarCube's configuration object, config.\n    See: http://www.motoslave.net/sugarcube/docs/#config"
 
 	def isEndTag(self, name, tag):
 		return (name == ('/' + tag) or name == ('end' + tag))
@@ -33,6 +33,27 @@ class Header (header.Header):
 				, 'id'
 				, 'update'
 			]
+
+	def passageTitleColor(self, passage):
+		additionalSpecialPassages = [
+				  'MenuOptions'
+				, 'MenuShare'
+				, 'MenuStory'
+				, 'PassageDone'
+				, 'PassageReady'
+				, 'StoryBanner'
+				, 'StoryCaption'
+			]
+		if passage.isStylesheet():
+			return ((111, 49, 83), (234, 123, 184))
+		elif passage.isScript():
+			return ((89, 66, 28), (226, 170, 80))
+		elif ('widget' in passage.tags):
+			return ((80, 106, 26), (134, 178, 44))
+		elif passage.isInfoPassage() or (passage.title in additionalSpecialPassages):
+			return ((28, 89, 74), (41, 214, 113))
+		elif passage.title == 'Start':
+			return ('#4ca333', '#4bdb24')
 
 	def passageChecks(self):
 		return super(Header, self).passageChecks()
