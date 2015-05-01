537d07cf59867de03e65324a536ead58b06ca2e9
Thomas Edwards <tmedwards@motoslave.net>
2015-05-01 10:45:58 -0500

    Added many new strings to the `strings` object.  This update should cover user-facing strings within SugarCube, sans fatal errors (since they tend to be both technical and unrecoverable).

--- src/strings.js
+++ src/strings.js
@@ -11,15 +11,66 @@ var	strings = {
 		// basics
 		identity : "game",
 
-		// saves
-		saves : {
-			// autosave autoload prompt
-			autoloadPrompt       : "There's an existing autosave.  Load it now or go to the start?",
-			autoloadPromptOK     : "Load autosave",
-			autoloadPromptCancel : "Go to start",
+		// ui-bar
+		uiBar : {
+			credits : 'Made with <a href="http://twinery.org/" target="_blank">Twine</a> &amp; <a href="http://www.motoslave.net/sugarcube/" target="_blank">SugarCube</a>',
+			toggle  : "Toggle the UI bar"
+		},
 
-			// saving disallowed message
-			disallowed : "Saving has been disallowed on this passage."
+		// saves dialog
+		saves : {
+			title          : "Saves",
+			disallowed     : "Saving has been disallowed on this passage.",
+			importLabel    : "Select a save file to load:",
+			incapable      : "Apologies! Your browser either lacks some of the capabilities required to support saves or has disabled them. The former may be solved by updating it to a newer version or by switching to a more modern browser. The latter may be solved by loosening its security restrictions or, perhaps, by viewing this %identity% via the HTTP protocol.",
+			loadFromDisk   : "Load from Disk\u2026",
+			purgeSlots     : "Purge Slots",
+			saveToDisk     : "Save to Disk\u2026",
+			unavailable    : "No save slots found\u2026",
+			autoSlotEmpty  : "—autosave slot empty—",
+			slotEmpty      : "—save slot empty—",
+			slotSave       : "Save",
+			slotLoad       : "Load",
+			slotDelete     : "Del.",
+			savedOn        : "Saved",
+			savedOnUnknown : "unknown"
+		},
+
+		// rewind dialog
+		rewind : {
+			title       : "Rewind",
+			turn        : "Turn",
+			unavailable : "No rewind points available\u2026"
+		},
+
+		// restart dialog
+		restart : {
+			title        : "Restart",
+			prompt       : "Are you sure that you want to restart? Unsaved progress will be lost.",
+			promptOK     : "OK",
+			promptCancel : "Cancel"
+		},
+
+		// options dialog
+		options : {
+			title       : "Options",
+			on          : "On",
+			off         : "Off",
+			promptOK    : "OK",
+			promptReset : "Reset to Defaults"
+		},
+
+		// share dialog
+		share : {
+			title : "Share",
+		},
+
+		// autosave autoload dialog
+		autoload : {
+			title        : "Autoload",
+			prompt       : "There's an existing autosave. Load it now or go to the start?",
+			promptOK     : "Load autosave",
+			promptCancel : "Go to start"
 		}
 	};
 
