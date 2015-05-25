c89acf908820243d8643b72513102fc1477c4b95
Thomas Edwards <tmedwards@motoslave.net>
2015-05-25 09:05:25 -0500

    Cleanup of the `Setting` API.

--- src/setting.js
+++ src/setting.js
@@ -12,10 +12,10 @@ var Setting = (function () { // eslint-disable-line no-unused-vars
 	"use strict";
 
 	var
-		controls = [],
+		_controls = [],
 
 		// Setup the Setting Types enumeration
-		Types    = Object.freeze({
+		Types     = Object.freeze({
 			Toggle : 0,
 			List   : 1
 		});
@@ -30,43 +30,50 @@ var Setting = (function () { // eslint-disable-line no-unused-vars
 		if (storage.hasItem("options")) {
 			var old = storage.getItem("options");
 			if (old !== null) {
+				/*
 				window.SugarCube.settings = settings = {};
 				Object.keys(old).forEach(function (name) {
 					settings[name] = old[name];
 				});
+				*/
+				window.SugarCube.settings = settings = Object.assign(settingsCreate(), old);
 			}
-			save();
+			settingsSave();
 			storage.removeItem("options");
 		}
 		/* /legacy */
 
-		load();
+		settingsLoad();
 	}
 
 
 	/*******************************************************************************************************************
-	 * Control Setup Functions
+	 * Controls Manipulation Functions
 	 ******************************************************************************************************************/
-	function addControl(type, name, definition) {
+	function controlsList() {
+		return _controls;
+	}
+
+	function controlsAdd(type, name, definition) {
 		/*
-		 * definition = {
-		 *     label    : (both:string),
-		 *     default  : (toggle:boolean) | (list:string/number), // system defaults: false or list[0],
-		 *     list     : (list:array),
-		 *     callback : (both:function)
-		 * }
+			definition = {
+				label    : (both:string),
+				default  : (toggle:boolean) | (list:string/number), // system defaults: false or list[0],
+				list     : (list:array),
+				callback : (both:function)
+			}
 		 */
 		if (arguments.length < 3) {
 			var errors = [];
 			if (arguments.length < 1) { errors.push("type"); }
 			if (arguments.length < 2) { errors.push("name"); }
 			if (arguments.length < 3) { errors.push("definition"); }
-			throw new Error("Setting.addControl missing parameters, no " + errors.join(" or ") + " specified");
+			throw new Error("missing parameters, no " + errors.join(" or ") + " specified");
 		}
 		if (typeof definition !== "object") {
-			throw new Error("Setting.addControl definition parameter must be an object");
+			throw new Error("definition parameter must be an object");
 		}
-		if (hasControl(name)) {
+		if (controlsHas(name)) {
 			throw new Error('cannot clobber existing setting "' + name + '"');
 		}
 		var control = {
@@ -103,19 +110,19 @@ var Setting = (function () { // eslint-disable-line no-unused-vars
 		if (typeof definition.callback === "function") {
 			control.callback = definition.callback;
 		}
-		controls.push(control);
+		_controls.push(control);
 	}
 
-	function addToggle(name, label, defaultValue, callback) {
-		addControl(Types.Toggle, name, {
+	function controlsAddToggle(name, label, defaultValue, callback) {
+		controlsAdd(Types.Toggle, name, {
 			label    : label,
 			default  : !!defaultValue,
 			callback : callback
 		});
 	}
 
-	function addList(name, label, list, defaultValue, callback) {
-		addControl(Types.List, name, {
+	function controlsAddList(name, label, list, defaultValue, callback) {
+		controlsAdd(Types.List, name, {
 			label    : label,
 			list     : list,
 			default  : defaultValue,
@@ -123,72 +130,79 @@ var Setting = (function () { // eslint-disable-line no-unused-vars
 		});
 	}
 
-	function removeControl(name) {
-		for (var i = 0; i < controls.length; i++) {
-			if (controls[i].name === name) {
-				controls.splice(i, 1);
-				remove(name);
-				break;
-			}
-		}
+	function controlsIsEmpty() {
+		return _controls.length === 0;
 	}
 
-	function hasControl(name) {
-		return controls.some(function (control) {
+	function controlsHas(name) {
+		return _controls.some(function (control) {
 			return control.name === this;
 		}, name);
 	}
 
-	function isEmpty() {
-		return controls.length === 0;
+	function controlsDelete(name) {
+		for (var i = 0; i < _controls.length; i++) {
+			if (_controls[i].name === name) {
+				_controls.splice(i, 1);
+				settingsDelete(name);
+				break;
+			}
+		}
 	}
 
 
 	/*******************************************************************************************************************
 	 * `settings` Object Manipulation Functions
 	 ******************************************************************************************************************/
-	function save() {
+	function settingsCreate() {
+		return Object.create(null);
+	}
+
+	function settingsSave() {
 		return storage.setItem("settings", settings);
 	}
 
-	function load() {
+	function settingsLoad() {
 		var obj = storage.getItem("settings");
 		if (obj !== null) {
-			window.SugarCube.settings = settings = {};
+			/*
+			window.SugarCube.settings = settings = settingsCreate();
 			Object.keys(obj).forEach(function (name) {
 				settings[name] = obj[name];
 			});
+			*/
+			window.SugarCube.settings = settings = Object.assign(settingsCreate(), obj);
 		}
 	}
 
-	function clear() {
+	function settingsClear() {
 		if (!storage.removeItem("settings")) {
 			throw new Error("unknown error, cannot update settings store");
 		}
-		window.SugarCube.settings = settings = {};
+		window.SugarCube.settings = settings = settingsCreate();
 	}
 
-	function reset() {
-		clear();
-		controls.forEach(function (control) {
+	function settingsReset() {
+		settingsClear();
+		_controls.forEach(function (control) {
 			settings[control.name] = control.default;
 		});
 	}
 
-	function has(name) {
+	function settingsHas(name) {
 		return settings.hasOwnProperty(name);
 	}
 
-	function get(name) {
+	function settingsGet(name) {
 		return settings[name];
 	}
 
-	function set(name, value) {
+	function settingsSet(name, value) {
 		settings[name] = value;
 	}
 
-	function remove(name) {
-		if (has(name)) {
+	function settingsDelete(name) {
+		if (settingsHas(name)) {
 			delete settings[name];
 		}
 	}
@@ -198,26 +212,32 @@ var Setting = (function () { // eslint-disable-line no-unused-vars
 	 * Exports
 	 ******************************************************************************************************************/
 	return Object.freeze(Object.defineProperties({}, {
-		controls      : { value : controls },
-		Types         : { value : Types },
+		// Enumeration
+		Types    : { value : Types },
 		// Initialization
-		init          : { value : init },
-		// Control Functions
-		addControl    : { value : addControl },
-		addToggle     : { value : addToggle },
-		addList       : { value : addList },
-		deleteControl : { value : removeControl },
-		hasControl    : { value : hasControl },
-		isEmpty       : { value : isEmpty },
+		init     : { value : init },
+		// Controls Manipulation Functions
+		controls : {
+			value : Object.freeze(Object.defineProperties({}, {
+				list      : { value : controlsList },
+				add       : { value : controlsAdd },
+				addToggle : { value : controlsAddToggle },
+				addList   : { value : controlsAddList },
+				isEmpty   : { value : controlsIsEmpty },
+				has       : { value : controlsHas },
+				delete    : { value : controlsDelete }
+			}))
+		},
 		// `settings` Object Manipulation Functions
-		save          : { value : save },
-		load          : { value : load },
-		clear         : { value : clear },
-		reset         : { value : reset },
-		has           : { value : has },
-		get           : { value : get },
-		set           : { value : set },
-		delete        : { value : remove }
+		create : { value : settingsCreate },
+		save   : { value : settingsSave },
+		load   : { value : settingsLoad },
+		clear  : { value : settingsClear },
+		reset  : { value : settingsReset },
+		has    : { value : settingsHas },
+		get    : { value : settingsGet },
+		set    : { value : settingsSet },
+		delete : { value : settingsDelete }
 	}));
 
 }());
