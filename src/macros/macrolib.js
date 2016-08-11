8f7662c568c2d7646c597ab074d2ef8f76cda978
Thomas Edwards <tmedwards@motoslave.net>
2016-08-11 09:08:48 -0500

    Added the `<<switch>>` macro and changed `<<if>>` to enforce that `<<else>>` must be the final clause.

--- src/macros/macrolib.js
+++ src/macros/macrolib.js
@@ -401,6 +401,9 @@
 					/* eslint-disable prefer-template */
 					switch (this.payload[i].name) {
 					case 'else':
+						if (i + 1 !== len) {
+							return this.error('<<else>> must be the final clause');
+						}
 						if (this.payload[i].args.raw.length > 0) {
 							if (/^\s*if\b/i.test(this.payload[i].args.raw)) {
 								return this.error(
@@ -489,6 +492,123 @@
 			catch (e) {
 				return this.error(`bad conditional expression in <<${i === 0 ? 'if' : 'elseif'}>> clause`
 					+ `${i > 0 ? ' (#' + i + ')' : ''}: ${e.message}`); // eslint-disable-line prefer-template
+			}
+		}
+	});
+
+	/*
+		<<switch>>, <<case>>, & <<default>>
+	*/
+	Macro.add('switch', {
+		skipArg0 : true,
+		tags     : ['case', 'default'],
+
+		handler() {
+			if (this.args.full.length === 0) {
+				return this.error('no expression specified');
+			}
+
+			const len = this.payload.length;
+			let result;
+
+			// if (len === 1 || !this.payload.some(p => p.name === 'case')) {
+			if (len === 1) {
+				return this.error('no cases specified');
+			}
+
+			try {
+				result = Scripting.evalJavaScript(this.args.full);
+			}
+			catch (e) {
+				return this.error(`bad evaluation: ${e.message}`);
+			}
+
+			const
+				debugView = this.debugView; // cache it now, to be modified later
+			let
+				i       = 1,
+				success = false;
+
+			// Initial debug view setup for `<<switch>>`.
+			debugView
+				.modes({
+					nonvoid : false,
+					hidden  : true
+				});
+
+			for (/* empty */; i < len; ++i) {
+				// Sanity checks.
+				switch (this.payload[i].name) {
+				case 'default':
+					if (i + 1 !== len) {
+						return this.error('<<default>> must be the final case');
+					}
+					if (this.payload[i].args.length > 0) {
+						return this.error(
+							`<<default>> does not accept values, invalid: ${this.payload[i].args.raw}`
+						);
+					}
+					break;
+
+				default:
+					if (this.payload[i].args.length === 0) {
+						return this.error(`no value(s) specified for <<${this.payload[i].name}>> (#${i})`);
+					}
+					break;
+				}
+
+				// Custom debug view setup for the current case.
+				if (Config.debug) {
+					this
+						.createDebugView(this.payload[i].name, this.payload[i].source)
+						.modes({ nonvoid : false });
+				}
+
+				// Case test(s).
+				if (this.payload[i].name === 'default' || this.payload[i].args.some(v => v === result)) {
+					success = true;
+					new Wikifier(this.output, this.payload[i].contents);
+					break;
+				}
+				else if (Config.debug) {
+					// Custom debug view setup for a failed case.
+					this.debugView.modes({
+						hidden  : true,
+						invalid : true
+					});
+				}
+			}
+
+			// Custom debug view setup for the remaining cases.
+			if (Config.debug) {
+				for (++i; i < len; ++i) {
+					this
+						.createDebugView(this.payload[i].name, this.payload[i].source)
+						.modes({
+							nonvoid : false,
+							hidden  : true,
+							invalid : true
+						});
+				}
+
+				/*
+					Finalize the debug view for `<<switch>>` and fake a debug view for `<</switch>>`.
+					We do both as a quick indicator of if any of the cases matched and the latter
+					to aid the checking of nesting.
+				*/
+				debugView
+					.modes({
+						nonvoid : false,
+						hidden  : true, // !success,
+						invalid : !success
+					});
+				this
+					.createDebugView(`/${this.name}`, `<</${this.name}>>`)
+					.modes({
+						nonvoid : false,
+						hidden  : true, // !success,
+						invalid : !success
+					});
 			}
 		}
 	});
