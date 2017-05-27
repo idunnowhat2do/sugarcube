eef7c644a7cacae941928a043b1ac14ce870f525
Thomas Edwards <tmedwards@motoslave.net>
2017-05-27 00:57:39 -0500

    [Update] Refactored `Has` feature tests for robustness and removed the Web Storage tests.  The Web Storage tests were moved to their `SimpleStore` adapter, as that's the only place they're used.

--- src/lib/has.js
+++ src/lib/has.js
@@ -11,50 +11,53 @@
 var Has = (() => { // eslint-disable-line no-unused-vars, no-var
 	'use strict';
 
-	function webStorageIsOk(store) {
+	/*
+		NOTE: The aggressive try/catch feature tests are necessitated by implementation
+		      bugs in various browsers.
+	*/
+
+	// HTML5 Audio Element API Test
+	const hasAudioElement = (() => {
 		try {
-			if (store != null && store.length >= 0) { // lazy equality for null
-				const tkey = 'SugarCube.WebStorage.Test';
-				const tval = '1701 Guilty Scott';
-
-				store.setItem(tkey, tval);
-
-				if (store.getItem(tkey) === tval) {
-					store.removeItem(tkey);
-					return true;
-				}
-			}
+			return typeof document.createElement('audio').canPlayType === 'function';
 		}
 		catch (ex) { /* no-op */ }
 
 		return false;
-	}
+	})();
+
+	// File API Test
+	const hasFile = (() => {
+		try {
+			return 'Blob' in window &&
+				'File' in window &&
+				'FileList' in window &&
+				'FileReader' in window &&
+				!Browser.isMobile.any() &&
+				(!Browser.isOpera || Browser.operaVersion >= 15);
+		}
+		catch (ex) { /* no-op */ }
+
+		return false;
+	})();
+
+	// Geolocation API Test
+	const hasGeolocation = (() => {
+		try {
+			return 'geolocation' in navigator &&
+				typeof navigator.geolocation.getCurrentPosition === 'function' &&
+				typeof navigator.geolocation.watchPosition === 'function';
+		}
+		catch (ex) { /* no-op */ }
+
+		return false;
+	})();
+
 
 	// Module Exports.
 	return Object.freeze({
-		audio : typeof document.createElement('audio').canPlayType === 'function',
-
-		/*
-			It's probably safe to assume the existence of `Blob` by the existence of `File`.
-		*/
-		fileAPI : 'File' in window && 'FileList' in window && 'FileReader' in window
-			&& !Browser.isMobile.any() && (!Browser.isOpera || Browser.operaVersion >= 15),
-
-		geolocation : 'geolocation' in navigator
-			&& typeof navigator.geolocation.getCurrentPosition === 'function'
-			&& typeof navigator.geolocation.watchPosition === 'function',
-
-		/*
-			The extended Web Storage testing (`webStorageIsOk()`) is required by implementation
-			bugs in various browsers.
-
-			Caveats by browser:
-				Firefox bug #748620 [https://bugzilla.mozilla.org/show_bug.cgi?id=748620].
-
-				The iOS browser core will throw an exception on `setItem()` calls when in
-				private browsing mode.
-		*/
-		localStorage   : 'localStorage' in window && webStorageIsOk(window.localStorage),
-		sessionStorage : 'sessionStorage' in window && webStorageIsOk(window.sessionStorage)
+		audio       : hasAudioElement,
+		fileAPI     : hasFile,
+		geolocation : hasGeolocation
 	});
 })();
